c9f370e50520d3f8ae826274f1f2ff078aab77f6
tseeta <66010362@kmitl.ac.th>
2026-09-23 16:05:39 +0700

    cell03

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+else:
+    i = 0
+    while i <= 10:
+        line = f"Table de {i}: "
+        j = 0
+        while j <= 10:
+            line += str(i * j) + " "
+            j += 1
+        print(line.strip())
+        i += 1
