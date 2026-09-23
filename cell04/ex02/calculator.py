1a6eb7bac7e306e651b1841e14c000996093db82
tseeta <66010362@kmitl.ac.th>
2026-09-23 16:17:40 +0700

    cell04

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+first = float(input("Give me the first number: "))
+second = float(input("Give me the second number: "))
+
+print("Thank you!")
+print(f"{first:g} + {second:g} = {first + second:g}")
+print(f"{first:g} - {second:g} = {first - second:g}")
+print(f"{first:g} / {second:g} = {first / second:g}")
+print(f"{first:g} * {second:g} = {first * second:g}")
