9897e7d5404b42323092f6519ffb5b5c4d100e4a
tseeta <66010362@kmitl.ac.th>
2026-09-23 17:32:57 +0700

    cell05

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+if len(sys.argv) == 3:
+    keyword = sys.argv[1]
+    text = sys.argv[2]
+    count = len(re.findall(keyword, text))
+    if count > 0:
+        print(count)
+    else:
+        print("none")
+else:
+    print("none")
